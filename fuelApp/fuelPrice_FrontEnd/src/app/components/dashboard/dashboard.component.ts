4ff30905cdffc91ccd8736e8f8a79540f814e92d
JRII_Ataka <jrejasii@gmail.com>
2023-02-24 08:40:08 -0600

    added dashboard required validators

--- fuelApp/fuelPrice_FrontEnd/src/app/components/dashboard/dashboard.component.ts
+++ fuelApp/fuelPrice_FrontEnd/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,10 +16,30 @@ export class DashboardComponent {
   userAccount:boolean = true;
   userRequest:boolean = true;
 
+  dashboardForm! : FormGroup 
+
   orderHistoryData = [
     {userId: 1, orderNum: 54321, gallonsReq: 6.0, deliAddress: "123 Wallstreet, Houston, Texas (TX)", deliDate: "02/20/2023", priceGal: "$40.00"},
     {userId: 2, orderNum: 24680, gallonsReq: 4.0, deliAddress: "987 Apple St, Dallas, Texas (TX)", deliDate: "02/21/2023", priceGal: "$40.00"}
   ];
+
+  constructor(private fb:FormBuilder){
+
+  }
+
+  ngOnInit(): void{
+    this.dashboardForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      addressOne: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zip: ['', Validators.required],
+      gallons: ['', Validators.required],
+      deliverDayte: ['', Validators.required]
+    })
+  
+  }
 
   hideTable(x: number){
   
