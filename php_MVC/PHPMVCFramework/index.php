381aeadc1268f732946f311d37920814c5ac5282
atakaii <jrejasii@gmail.com>
2023-05-30 10:49:50 -0500

    working on php MVC routing

--- php_MVC/PHPMVCFramework/index.php
+++ php_MVC/PHPMVCFramework/index.php
@@ -1,4 +1,16 @@
 <?php
 
+require_once __DIR__.'/vendor/autoload.php';
+use app\core\Application;
 
-echo "Hello World";
+$app = new Application();
+
+$app->router->get('/', function(){
+    return 'Hello World';
+});
+
+$app->router->get('/contact', function(){
+    return 'Contact';
+});
+
+$app->run();
---
+++ php_MVC/PHPMVCFramework/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'atakaii/phpmvc-framework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '641abcbd630506d79bd23a1ff55897a348c5c415',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'atakaii/phpmvc-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '641abcbd630506d79bd23a1ff55897a348c5c415',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
